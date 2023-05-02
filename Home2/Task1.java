38dc89e92b2a92d4d81bd8d77e1539e562f33aa5
Nikita <nikitaraserg@outlook.com>
2023-05-02 18:49:24 +0300

    Added Homework 2 Added Task1

---
+++ Home2/Task1.java
@@ -0,0 +1,38 @@
+package Home2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/*  
+    Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), 
+    и возвращает введенное значение. 
+    Ввод текста вместо числа не должно приводить к падению приложения, 
+    вместо этого, необходимо повторно запросить у пользователя ввод данных. 
+*/
+public class Task1 {
+    public static void main(String[] args) {
+        processEnteredDate();
+    }
+    public static float numberFloat() {
+        System.out.println("Введите дробное число через запятую:");
+        Scanner scanNum = new Scanner(System.in);
+        float num = scanNum.nextFloat();
+        System.out.println("Вы ввели:\n" + (num));
+        return num;
+    }
+    /**
+     * @return
+     */
+    public static float processEnteredDate(){
+        try{
+            numberFloat();
+        }
+        catch(InputMismatchException e){
+            System.out.println("Неверный ввод!");
+            processEnteredDate();
+    
+        }
+        return 0;
+    }
+    
+}
